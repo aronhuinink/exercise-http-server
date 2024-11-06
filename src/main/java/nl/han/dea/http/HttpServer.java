c87a6a69ae513a2ba644e0d0a6d3d0f3f76a6aaf
Aron Huinink <awj.huinink@student.han.nl>
2024-11-06 14:22:05 +0100

    opdracht 1 gefixt

--- src/main/java/nl/han/dea/http/HttpServer.java
+++ src/main/java/nl/han/dea/http/HttpServer.java
@@ -2,10 +2,17 @@ package nl.han.dea.http;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
-public class HttpServer {
+public class HttpServer implements Runnable {
 
     private int tcpPort;
+
+    private Socket acceptedSocket;
+
+    public HttpServer(Socket acceptedSocket) {
+        this.acceptedSocket = acceptedSocket;
+    }
 
     public HttpServer(int tcpPort) {
         this.tcpPort = tcpPort;
@@ -16,17 +23,37 @@ public class HttpServer {
     }
 
     public void startServer() {
-        try (
-                var serverSocket = new ServerSocket(this.tcpPort);
-        ) {
-            System.out.println("Server accepting requests on port " + tcpPort);
 
-            var acceptedSocket = serverSocket.accept();
-            var connectionHandler = new ConnectionHandler(acceptedSocket);
-            connectionHandler.handle();
+        HttpServer server = new HttpServer(tcpPort);
 
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+
+        while(true) {
+
+            try (
+                    var serverSocket = new ServerSocket(this.tcpPort);
+            ) {
+
+
+                System.out.println("Server accepting requests on port " + tcpPort);
+
+                Socket clientSocket = serverSocket.accept();
+
+                Thread t = new Thread(new HttpServer(clientSocket));
+                t.start();
+
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
+
+    }
+
+    @Override
+    public void run() {
+        System.out.println("running thread " + Thread.currentThread().getId());
+
+        var connectionHandler = new ConnectionHandler(acceptedSocket);
+        connectionHandler.handle();
     }
 }
